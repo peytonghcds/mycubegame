94297907a39129f4ba6468e56e289b6cbaa6de37
peytonghcds <102323269+peytonghcds@users.noreply.github.com>
2022-04-26 19:16:53 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+import pygame,random
+pygame.init()
+#sets up the game display
+window = pygame.display.set_mode ("Peytons fun game") (600,400)
+pygame.display.set_caption
+Yellow = (229,247,32)
+White = (255,255,255)
+Red=(222,40,40)
+Green = (40,222,40)
+Blue = (40,52,222)
+random.color
+bgcolor
+''''''
+(100,50,200)
+(10,200,700)
+bg_color = white 
+speed = 5
+
+cubeX = 250
+cubeY = 250
+cubeSize = 20
+
+run = True
+
+food = [random.randrange(1,500),random.randrange(1,500), 10, 10]
+Bfood = [random.randrange(1,500),random.randrange(1,500), 10, 10]
+
+
+
